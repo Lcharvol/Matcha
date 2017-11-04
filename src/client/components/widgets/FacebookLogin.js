2841541d83050b783f5dbb98d4fbbe937d62b94d
lcharvol <lcharvol@student.42.fr>
2017-11-04 14:27:01 +0100

    Add Facebook and Google Button

---
+++ src/client/components/widgets/FacebookLogin.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    width:100%;
+    height:50px;
+    border-radius:3px;
+    background-color: #3498db;
+    background: linear-gradient( 160deg, #649cbe 0%, #3498db  100%);
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.15);
+    cursor: pointer;
+    &:hover {
+        transition: all 60ms ease;
+    }
+`;
+
+const FacebookLoginContainer = styled.div`
+    display:flex;
+    margin-top:25px;
+    width:390px;
+`;
+
+const Logo = styled.i`
+    color:white;
+    font-size:1.3em;
+    margin-right:15px;
+`;
+
+const Text = styled.p`
+    color:white;
+`;
+
+const FacebookLogin = () => (
+    <FacebookLoginContainer>
+        <Button>
+            <Logo className="fa fa-facebook" aria-hidden="true" />
+            <Text>Sign in with Facebook</Text>
+        </Button>
+    </FacebookLoginContainer>
+);
+
+export default FacebookLogin;
---
+++ src/client/components/widgets/GoogleLogin.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    width:100%;
+    height:50px;
+    border-radius:3px;
+    background: linear-gradient( 160deg, #ea5e53 0%, #e74c3c  100%);
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.15);
+    cursor: pointer;
+    &:hover {
+        transition: all 60ms ease;
+    }
+`;
+
+const GoogleLoginContainer = styled.div`
+    display:flex;
+    margin-top:25px;
+    width:390px;
+`;
+
+const Logo = styled.i`
+    color:white;
+    font-size:1.3em;
+    margin-right:15px;
+`;
+
+const Text = styled.p`
+    color:white;
+`;
+
+const GoogleLogin = () => (
+    <GoogleLoginContainer>
+        <Button>
+            <Logo className="fa fa-google" aria-hidden="true" />
+            <Text>Sign in with Google</Text>
+        </Button>
+    </GoogleLoginContainer>
+);
+
+export default GoogleLogin;

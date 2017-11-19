284501aa654d91c90c5b0f91e2ed19fae58fcb0c
Lucas <charvolin0@gmail.com>
2017-11-19 22:56:17 +0100

    Users > Add sort/filter actions

--- src/client/actions/users.js
+++ src/client/actions/users.js
@@ -1,5 +1,17 @@
 export const LOAD_USERS = 'LOAD_USERS';
+export const FILTER_USERS = 'FILTER_USERS';
+export const SORT_USERS = 'SORT_USERS';
 
 export const loadUsers = users => {
   return { type: LOAD_USERS, users };
 };
+
+export const sortUsers = sortBy => ({
+  type: SORT_USERS,
+  sortBy,
+});
+
+export const filterUsers = filter => ({
+  type: FILTER_USERS,
+  filter,
+});

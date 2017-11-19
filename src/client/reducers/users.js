c3debc80c0016a7121b2d0cad61d971019eb4288
Lucas <charvolin0@gmail.com>
2017-11-19 22:56:00 +0100

    Users > Add sort/filter reducers

--- src/client/reducers/users.js
+++ src/client/reducers/users.js
@@ -1,9 +1,26 @@
-import { LOAD_USERS } from '../actions/users';
+import {
+  LOAD_USERS,
+  SORT_USERS,
+  FILTER_USERS
+} from '../actions/users';
 
-const reducer = (state = {}, action) => {
+const initialState = {
+  details: [],
+  sort: { by: 'name', order: 'asc' },
+  filter: '',
+};
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case SORT_USERS: {
+      const { by, order } = state.sort;
+      const newOrder = by === action.sortBy && order === 'asc' ? 'desc' : 'asc';
+      return { ...state, sort: { by: action.sortBy, order: newOrder } };
+    }
+    case FILTER_USERS:
+      return { ...state, filter: action.filter };
     case LOAD_USERS: {
-      return action.users;
+      return {...state, details: action.users.details};
     }
     default:
         return state;

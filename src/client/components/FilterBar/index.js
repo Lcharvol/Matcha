1ef1cd62f67cf30dafea7cbd87089341ab5587b5
lcharvol <charvolin0@google.com>
2017-11-19 13:32:55 +0100

    FilterBar > Add component

---
+++ src/client/components/FilterBar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display:flex;
+    margin-top:25px;
+    width:100%;
+    height:50px;
+`;
+
+const FilterBar = () => (
+    <Container>
+    </Container>
+);
+
+export default FilterBar;

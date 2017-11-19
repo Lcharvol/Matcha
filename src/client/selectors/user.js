aeda30dc9b0820fe1f1a23824f78a0b584ea20d6
Lucas <charvolin0@gmail.com>
2017-11-19 22:55:49 +0100

    Users > Add sort/filter selector

--- src/client/selectors/user.js
+++ src/client/selectors/user.js
@@ -1,3 +1,5 @@
 
 export const getUser = state => state.user;
-export const getUsers = state => state.users;
+export const getUsers = state => state.users.details;
+export const getFilter = state => state.users.filter;
+export const getSort = state => state.users.sort;

a1ed35fc7e6850b064b004e69d78d9bc83f4eed1
Allan BARRIELLE <abarriel@student.42.fr>
2017-12-04 18:42:55 +0100

    like is brooken

--- console.sh
+++ console.sh
@@ -1,1 +1,1 @@
-#find ./node_modules/webpack/. -type f -name "*.js" -exec sed -i '' 's/console.log.*$//g' {} \;
+find ./node_modules/webpack/. -type f -name "*.js" -exec sed -i '' 's/console.log.*$//g' {} \;

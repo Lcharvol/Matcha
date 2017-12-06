88b137ee45d09c5809b2c2d8292e38905b786432
Allan BARRIELLE <abarriel@student.42.fr>
2017-12-06 23:06:53 +0100

    clean

--- src/client/store.js
+++ src/client/store.js
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import axios from 'axios';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 
 import reducer from './reducers';
 import { CONNECTED_USER } from './actions/users';
 
-const logger = createLogger({
-  collapsed: true,
-  predicate: (getState, action) => action.type !== CONNECTED_USER
-});
+// const logger = createLogger({
+//   collapsed: true,
+//   predicate: (getState, action) => action.type !== CONNECTED_USER
+// });
 
 const asyncDispatchMiddleware = store => next => action => {
   let syncActivityFinished = false;
@@ -42,7 +42,7 @@ const configureStore = (initialState, io) =>
   createStore(
       reducer(io),
       initialState,
-      applyMiddleware(thunk, logger, asyncDispatchMiddleware, SocketIoMiddleware(io)),
+      applyMiddleware(thunk, asyncDispatchMiddleware, SocketIoMiddleware(io)),
     );
 
 export default configureStore;
